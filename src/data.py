d897181ba8895caa3eb8a6cc0e2fd09e551eac48
Mehmood Hassan <m.hassan.rahi@gmail.com>
2026-06-01 13:26:30 +0200

    feat: added CRUD for posts

---
+++ src/data.py
@@ -0,0 +1,18 @@
+text_posts = {
+    1: {
+        "title": "First Post",
+        "content": "This is the content of the first post."
+    },
+    2: {
+        "title": "Second Post",
+        "content": "This is the content of the second post."
+    },
+    3: {
+        "title": "Third Post",
+        "content": "This is the content of the third post."
+    },
+    4: {
+        "title": "Fourth Post",
+        "content": "This is the content of the fourth post."
+    },
+}
